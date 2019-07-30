61b47e13953eef00a0eaef60dbe2866b7374aecb
JORJONG <joris.de.jong@capgemini.com>
2019-07-30 11:49:20 +0200

    added stuff app.properties to gitignore. cleaned up some dead code. added some metadata for debugging sql queries.

--- src/main/java/com/hotelcalifornia/hotel/repository/BookingRepository.java
+++ src/main/java/com/hotelcalifornia/hotel/repository/BookingRepository.java
@@ -1,15 +1,9 @@
 package com.hotelcalifornia.hotel.repository;
 
-
-import com.hotelcalifornia.hotel.exceptions.EmptyRepoException;
-import com.hotelcalifornia.hotel.exceptions.NotFoundException;
 import com.hotelcalifornia.hotel.models.Booking;
-import com.hotelcalifornia.hotel.utils.EnvironmentSingleton;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Book;
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
