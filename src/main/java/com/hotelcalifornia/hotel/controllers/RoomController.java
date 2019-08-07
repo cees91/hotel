540be4eeca2475a732d90293ad22c8ae894ca4d7
ala-roshdieh <alaroshdieh@gmail.com>
2019-08-07 16:23:23 +0200

    deleted old util classes

--- src/main/java/com/hotelcalifornia/hotel/controllers/RoomController.java
+++ src/main/java/com/hotelcalifornia/hotel/controllers/RoomController.java
@@ -2,19 +2,14 @@ package com.hotelcalifornia.hotel.controllers;
 
 import com.hotelcalifornia.hotel.exceptions.BadRequestException;
 import com.hotelcalifornia.hotel.models.Room;
-import com.hotelcalifornia.hotel.repository.RoomRepository;
 import com.hotelcalifornia.hotel.services.RoomService;
-import com.hotelcalifornia.hotel.utils.CSVReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
 
-import java.lang.reflect.Array;
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @RestController()
@@ -25,13 +20,15 @@ public class RoomController {
     private RoomService service;
 
 
-    @RequestMapping(value = "", method = RequestMethod.POST)
-    public void addrooms(@RequestBody List<Room> rooms){
-         service.addRooms(rooms);
+    @RequestMapping(value = "new", method = RequestMethod.POST)
+    public void addRoom(@RequestBody Room room){
+         service.addRoom(room);
     }
+
 
     @RequestMapping(value = "", method = RequestMethod.DELETE, params = "roomId")
     public void deleteRoom(@RequestParam("roomId") long id) {
+        service.deleteRoomById(id);
     }
 
     @RequestMapping(value = "/id", method = RequestMethod.GET, params = "roomId")
@@ -40,7 +37,7 @@ public class RoomController {
         try {
             room = service.findRoom(id);
         } catch (Exception error) {
-            System.out.println(error);
+            System.out.println(error.getMessage());
         }
         return room;
     }
@@ -54,7 +51,7 @@ public class RoomController {
     public ArrayList<Room> getAllRooms() {
         ArrayList<Room> rooms = null;
         try {
-            rooms = service.getRooms();
+            rooms = service.getAllRooms();
         } catch (Exception error) {
             System.out.println(error);
         }
