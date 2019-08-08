892cdb2c952b12daa2c90246f4327adf7f2ce567
Cees Jansen <cees.a.jansen@capgemini.com>
2019-08-08 10:19:07 +0200

    added login controller

--- src/main/java/com/hotelcalifornia/hotel/models/Room.java
+++ src/main/java/com/hotelcalifornia/hotel/models/Room.java
@@ -9,6 +9,7 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -38,8 +39,8 @@ public class Room {
     public Room() { }
 
 
-    @ManyToMany
-    private Set<Booking> bookings;
+    @ManyToMany(mappedBy = "bookedRooms")
+    private List<Booking> bookings;
 
 
     public long getId() {
